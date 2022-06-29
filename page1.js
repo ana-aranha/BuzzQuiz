21dbd5cf0c27ecd485caa28abbe370e188c7f4d1
Rogerio Junior <rogerio.junior1016@gmail.com>
2022-06-29 16:51:22 -0300

    Non quizz layout

--- page1.js
+++ page1.js
@@ -1,35 +1,42 @@
 //Lista de Quizzes
-let linkBuzzQuiz = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
-let quizzes
-
-function getQuiz(){
-    let promise = axios.get(linkBuzzQuiz)
-    promise.then(quizLoaded)
+let linkBuzzQuiz = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+let quizzes;
+let userStorage = [];
+let prepareStorage = JSON.stringify(userStorage);
+localStorage.setItem("quizzes", prepareStorage);
+localStorage.setItem("quizzes", prepareStorage);
+function getQuiz() {
+  let promise = axios.get(linkBuzzQuiz);
+  promise.then(renderQuiz);
 }
 
-function quizLoaded(responsta){
-    quizzes = responsta.data
-    document.querySelector("main").innerHTML=`
-    <div class='userQuizzes'>
-    TESTE
-    </div>
-    <div class='otherQuizzes'></div>`
-    renderQuiz()
+function yourQuizzes() {
+  if (userStorage.length === 0) {
+    document.querySelector("main").innerHTML = `
+        <div class='userQuizzes'>
+        <h1>Você não criou nenhum</br>quizz ainda :(</h1>
+        <button>Criar Quizz</button>
+        </div>
+        <div class='otherQuizzes'></div>`;
+  }
 }
 
 function isImage(url) {
-    return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
-  }
+  return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+}
 
-function renderQuiz(){
-    for(let i=0;i<quizzes.length-2;i++){
-        if(isImage(quizzes[i].image)){
-        document.querySelector(".otherQuizzes").innerHTML += `
+function renderQuiz(resposta) {
+  quizzes = resposta.data;
+  for (let i = 0; i < quizzes.length - 2; i++) {
+    if (isImage(quizzes[i].image)) {
+      document.querySelector(".otherQuizzes").innerHTML += `
         <div class = 'quizzStyle' onclick='openQuizz(this)'>
         <img src='${quizzes[i].image}'>
         <p>${quizzes[i].title}</p>
-        <div>`}
+        <div>`;
     }
+  }
 }
 
-getQuiz()
+yourQuizzes();
+getQuiz();
